f9c6dca9ed208f3e8d1c9f72db0fe9db383c1356
Brint E. Kriebel <bekit@cyngn.com>
2015-01-08 14:36:45 -0800

    releasetools: Store the build.prop file in the OTA zip
    This file is often used to read information about the update contained
    in the OTA. Place it in the update so it can be used by scripts. Add
    build.prop to incrementals too
    
    The file is not added to the updater-script, so it will not be placed
    onto the actual system.
    
    [GabrieleM: Adapt for A/B OTA packages]
    
    Change-Id: I88044796cbe8f199ca02df2840fd944cba2c73fa
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -1058,6 +1058,9 @@ endif;
   script.AddToZip(input_zip, output_zip, input_path=OPTIONS.updater_binary)
   metadata["ota-required-cache"] = str(script.required_cache)
 
+  common.ZipWriteStr(output_zip, "system/build.prop",
+                     ""+input_zip.read("SYSTEM/build.prop"))
+
   # We haven't written the metadata entry, which will be done in
   # FinalizeMetadata.
   common.ZipClose(output_zip)
@@ -1850,6 +1853,9 @@ endif;
   else:
     script.AddToZip(target_zip, output_zip, input_path=OPTIONS.updater_binary)
   metadata["ota-required-cache"] = str(script.required_cache)
+
+  common.ZipWriteStr(output_zip, "system/build.prop",
+                     ""+target_zip.read("SYSTEM/build.prop"))
 
   # We haven't written the metadata entry yet, which will be handled in
   # FinalizeMetadata().
