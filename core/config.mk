c2d0f5fad59c86461e5b16f2a0500f3e6252a237
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2018-11-03 20:35:11 +0530

    Revert "don't include custom sepolices yet"
    This reverts commit 44c15d74ce853e1b9f6eb76de8da047adbffa9c7.

--- core/config.mk
+++ core/config.mk
@@ -1152,7 +1152,7 @@ endif
 ifneq ($(CUSTOM_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-#$(eval include device/aosp/sepolicy/common/sepolicy.mk)
+$(eval include device/superior/sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk
