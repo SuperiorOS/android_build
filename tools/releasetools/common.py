2449bfdf15ed8050f68d9fe91243009b5ef61c90
Nicolas Dhouailly <ndhouailly@gmail.com>
2018-11-11 13:56:31 +0100

    Display progress of system & vendor images when flashing
    * Previously system image was displayed twice instead of displaying system and vendor images progress
    
    Change-Id: Ic4e427cb6a34f93ab48c8e2a92942ac6b341d60b
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -2691,7 +2691,10 @@ class BlockDifference(object):
     if not self.src:
       # write the output unconditionally
       script.Print(" ")
-      script.Print("Flashing SuperiorOS System files...")
+      script.Print("Flashing SuperiorOS %s files..." % (self.partition,))
+    else:
+      script.Print(" ")
+      script.Print("Flashing SuperiorOS %s files after verification." % (self.partition,))
 
     if progress:
       script.ShowProgress(progress, 0)
@@ -2791,6 +2794,8 @@ class BlockDifference(object):
 
   def WritePostInstallVerifyScript(self, script):
     partition = self.partition
+    script.Print(" ")
+    script.Print('Verifying SuperiorOS %s files...' % (partition,))
     # Unlike pre-install verification, clobbered_blocks should not be ignored.
     ranges = self.tgt.care_map
     ranges_str = ranges.to_string_raw()
@@ -2808,7 +2813,11 @@ class BlockDifference(object):
               self.device, ranges_str,
               self._HashZeroBlocks(self.tgt.extended.size())))
       script.Print(" ")
-      script.Print("Verified SuperiorOS System files...")
+      script.Print('Verified SuperiorOS %s files.' % (partition,))
+      if partition == "system":
+        code = ErrorCode.SYSTEM_NONZERO_CONTENTS
+      else:
+        code = ErrorCode.VENDOR_NONZERO_CONTENTS
       script.AppendExtra(
           'else\n'
           '  abort("E%d: %s partition has unexpected non-zero contents after '
@@ -2816,7 +2825,7 @@ class BlockDifference(object):
           'endif;' % (code, partition))
     else:
       script.Print(" ")
-      script.Print("Verified SuperiorOS System files...")
+      script.Print('Verified SuperiorOS %s files.' % (partition,))
 
     if partition == "system":
       code = ErrorCode.SYSTEM_UNEXPECTED_CONTENTS
