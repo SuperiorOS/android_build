3465d17b711f2d168bd918cdd07a2b496f9762a6
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-01-22 15:50:52 +0100

    Don't export BUILD_NUMBER in envsetup
    * The value is meant to be set only before running an /official/ build
    * This allows us to execute consecutive make commands without
      running kati for every invocation
    
    Change-Id: Ifa94b839f30c7260009c8a3c91c202b50e28022e
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -144,7 +144,6 @@ function check_product()
     fi
     if (echo -n $1 | grep -q -e "^aosp_") ; then
         SUPERIOR_BUILD=$(echo -n $1 | sed -e 's/^superior_//g')
-        export BUILD_NUMBER=$( (date +%s%N ; echo $SUPERIOR_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10 )
     else
         SUPERIOR_BUILD=
     fi
