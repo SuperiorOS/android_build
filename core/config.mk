AOSP-10/build

core: Only include custom sepolicy if repo exists
Change-Id: If799106d2da6d8c303430e316edff365a22f7c99

core/config.mk
@@ -1191,9 +1191,11 @@ DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
 
 ifneq ($(CUSTOM_BUILD),)
+ifneq ($(wildcard device/custom/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
 $(eval include device/custom/sepolicy/common/sepolicy.mk)
+endif
 endif
 
 # Include any vendor specific config.mk file

SuperiorOS/android_build

Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

core/config.mk
@@ -1184,9 +1184,11 @@ DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
 
 ifneq ($(SUPERIOR_BUILD),)
+ifneq ($(wildcard device/superior/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
 $(eval include device/superior/sepolicy/common/sepolicy.mk)
+endif
 endif
 
 # Include any vendor specific config.mk file