AICP/build

build: add maintainers to build.prop
Change-Id: I9dc1c6a95c441829b5900ff9c83aa32540ddabf1

tools/buildinfo.sh
@@ -85,6 +85,9 @@ if [ "$TARGET_UNIFIED_DEVICE" == "" ] ; then
   if [ -n "$BUILD_THUMBPRINT" ] ; then
     echo "ro.build.thumbprint=$BUILD_THUMBPRINT"
   fi
+  if [ -n "$DEVICE_MAINTAINERS" ] ; then
+    echo "ro.aicp.maintainer=$DEVICE_MAINTAINERS"
+  fi
 fi
 echo "ro.build.characteristics=$TARGET_AAPT_CHARACTERISTICS"
 

SuperiorOS/android_build

Change-Id: I2a25b4b8c04f5c2624107d02b3489a6d154a0cd1
Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

tools/buildinfo.sh
@@ -63,4 +63,8 @@ fi
 
 echo "ro.superior.device=$SUPERIOR_DEVICE"
 
+if [ -n "$DEVICE_MAINTAINERS" ] ; then
+  echo "ro.superior.maintainer=$DEVICE_MAINTAINERS"
+fi
+
 echo "# end build properties"