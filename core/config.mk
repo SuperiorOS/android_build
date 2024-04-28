9b8354464233d2f6158f614f23be599ec0225568
Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
2024-04-28 20:46:55 +0530

    build: Make BoardConfigSuperior optional
    Change-Id: I51b21037c8f009bedc80cfd045a7df5a169ece00
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -443,7 +443,7 @@ FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 -include vendor/extra/BoardConfigExtra.mk
 ifneq ($(SUPERIOR_BUILD),)
-include vendor/superior/config/BoardConfigSuperior.mk
+-include vendor/superior/config/BoardConfigSuperior.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
