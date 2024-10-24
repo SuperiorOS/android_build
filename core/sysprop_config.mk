bionic-linux/android_build_make

Set system property ro.product.page_size
When target sets TARGET_BOOTS_16K, it should boot
with 16KB page size. Adding system property based on TARGET_BOOTS_16K.

Test: m Vts16KPageSizeTest && atest -c Vts16KPageSizeTest
Flag: TEST_ONLY
Bug: 350993987
Change-Id: I72c55261ad8d67dfa6be7374e1acd6baddbb3342

core/sysprop_config.mk
@@ -50,6 +50,12 @@ else
 ADDITIONAL_PRODUCT_PROPERTIES += ro.product.build.16k_page.enabled=false
 endif
 
+ifeq ($(TARGET_BOOTS_16K),true)
+ADDITIONAL_SYSTEM_PROPERTIES += ro.product.page_size=16384
+else
+ADDITIONAL_SYSTEM_PROPERTIES += ro.product.page_size=4096
+endif
+
 # Enable core platform API violation warnings on userdebug and eng builds.
 ifneq ($(TARGET_BUILD_VARIANT),user)
 ADDITIONAL_SYSTEM_PROPERTIES += persist.debug.dalvik.vm.core_platform_api_policy=just-warn

SuperiorOS/android_build

core/sysprop_config.mk
@@ -50,6 +50,12 @@ else
 ADDITIONAL_PRODUCT_PROPERTIES += ro.product.build.16k_page.enabled=false
 endif
 
+ifeq ($(TARGET_BOOTS_16K),true)
+ADDITIONAL_PRODUCT_PROPERTIES += ro.product.page_size=16384
+else
+ADDITIONAL_PRODUCT_PROPERTIES += ro.product.page_size=4096
+endif
+
 # Enable core platform API violation warnings on userdebug and eng builds.
 ifneq ($(TARGET_BUILD_VARIANT),user)
 ADDITIONAL_SYSTEM_PROPERTIES += persist.debug.dalvik.vm.core_platform_api_policy=just-warn